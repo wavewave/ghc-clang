1c66f19dcf8534668f16ea11ca44e1039c04e892
Ian-Woo Kim <ianwookim@gmail.com>
2022-05-25 13:31:44 -0700

    override pkgsHostTarget.targetPackages, pkgsBuildTarget.targetPackages

--- shell.nix
+++ shell.nix
@@ -5,6 +5,18 @@ let
         haskellPackages = prev.haskell.packages.ghc8107.override {
           ghc = final.buildPackages.haskell.compiler.ghc8107.override {
             useLLVM = true;
+            stdenv = prev.clangStdenv;
+            llvmPackages = final.llvmPackages_12;
+            targetPackages = prev.targetPackages.extend
+              (self: super: { stdenv = prev.clangStdenv; });
+            pkgsHostTarget = prev.pkgsHostTarget.extend (self: super: {
+              targetPackages = super.targetPackages.extend
+                (sself: super: { stdenv = prev.clangStdenv; });
+            });
+            pkgsBuildTarget = prev.pkgsBuildTarget.extend (self: super: {
+              targetPackages = super.targetPackages.extend
+                (sself: super: { stdenv = prev.clangStdenv; });
+            });
           };
           stdenv = prev.clangStdenv;
         };
@@ -16,5 +28,3 @@ in (pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
 
   buildInputs = [ hsenv ];
 }
-
-#pkgs.clangStdenv.mkDerivation { name = "test-clangStdenv-shell"; }
