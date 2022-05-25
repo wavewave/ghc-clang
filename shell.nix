f72d9e52b526263abb7a75504701a9f2052e808d
Ian-Woo Kim <ianwookim@gmail.com>
2022-05-25 13:54:01 -0700

    myStdenv = llvmPackages_12.stdenv

--- shell.nix
+++ shell.nix
@@ -1,26 +1,28 @@
 let
   pkgs = import <nixpkgs> {
     overlays = [
-      (final: prev: {
-        haskellPackages = prev.haskell.packages.ghc8107.override {
-          ghc = final.buildPackages.haskell.compiler.ghc8107.override {
-            useLLVM = true;
-            stdenv = prev.clangStdenv;
-            llvmPackages = final.llvmPackages_12;
-            targetPackages = prev.targetPackages.extend
-              (self: super: { stdenv = prev.clangStdenv; });
-            pkgsHostTarget = prev.pkgsHostTarget.extend (self: super: {
-              targetPackages = super.targetPackages.extend
-                (sself: super: { stdenv = prev.clangStdenv; });
-            });
-            pkgsBuildTarget = prev.pkgsBuildTarget.extend (self: super: {
-              targetPackages = super.targetPackages.extend
-                (sself: super: { stdenv = prev.clangStdenv; });
-            });
+      (final: prev:
+        let myStdenv = final.llvmPackages_12.stdenv;
+        in {
+          haskellPackages = prev.haskell.packages.ghc8107.override {
+            ghc = final.buildPackages.haskell.compiler.ghc8107.override {
+              useLLVM = true;
+              stdenv = myStdenv;
+              llvmPackages = final.llvmPackages_12;
+              targetPackages = prev.targetPackages.extend
+                (self: super: { stdenv = myStdenv; });
+              pkgsHostTarget = prev.pkgsHostTarget.extend (self: super: {
+                targetPackages = super.targetPackages.extend
+                  (sself: super: { stdenv = myStdenv; });
+              });
+              pkgsBuildTarget = prev.pkgsBuildTarget.extend (self: super: {
+                targetPackages = super.targetPackages.extend
+                  (sself: super: { stdenv = myStdenv; });
+              });
+            };
+            stdenv = myStdenv;
           };
-          stdenv = prev.clangStdenv;
-        };
-      })
+        })
     ];
   };
   hsenv = pkgs.haskellPackages.ghcWithPackages (p: [ p.template-haskell ]);
